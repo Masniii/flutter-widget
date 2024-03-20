9df496ae3bd1a879481f365b774222639572fcac
new name <2211102441100@umkt.ac.id>
2024-03-20 19:06:40 +0800

    Container

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,38 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [LeafBorderText(), LeafBorderText(), LeafBorderText()],
+        ),
+      ),
+    );
+  }
+}
+
+class LeafBorderText extends StatelessWidget {
+  const LeafBorderText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 219, 143, 72),
+          border: Border.all(),
+          borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          "Halo Dunia",
+          style: TextStyle(
+              color: Color.fromARGB(255, 27, 124, 153),
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
