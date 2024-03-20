92adb182278bfd4ee8bf09a649e99507d62a85f8
new name <2211102441100@umkt.ac.id>
2024-03-20 19:18:27 +0800

    ListView

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,67 +5,25 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            LeafBorderText(
-              text: 'Gambar 1',
-              imgCode: '1',
-            ),
-            LeafBorderText(
-              text: 'Gambar 2',
-              imgCode: '2',
-            ),
-            LeafBorderText(
-              imgCode: '0',
-            )
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView Example'),
         ),
-      ),
-    );
-  }
-}
-
-class LeafBorderText extends StatelessWidget {
-  final String imgCode;
-  final String text;
-  const LeafBorderText(
-      {super.key, this.text = 'halo dunia', this.imgCode = '1'});
-
-  void handleButton() {
-    print("Saya dipencet");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Color.fromARGB(255, 219, 143, 72),
-          border: Border.all(),
-          borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(20), topRight: Radius.circular(20))),
-      child: Padding(
-        padding: EdgeInsets.all(9),
-        child: Row(
-          children: [
-            Image(
-                image: NetworkImage(
-                    'https://picsum.photos/50/50/?random=$imgCode')),
-            const SizedBox(
-              width: 8,
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
             ),
-            Icon(Icons.settings_accessibility),
-            Text(
-              text,
-              style: const TextStyle(
-                  color: Color.fromARGB(255, 27, 124, 153),
-                  fontWeight: FontWeight.bold),
+            ListTile(
+              leading: Icon(Icons.photo),
+              title: Text('Album'),
             ),
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    onPressed: handleButton, child: const Text('Pencet Saya')))
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
           ],
         ),
       ),
